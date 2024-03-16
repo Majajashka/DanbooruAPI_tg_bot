c876f8db3de1fe123d1c810f2125e4f02f2f8c7a
Majajashka <96516033+Majajashka@users.noreply.github.com>
2024-03-17 02:39:07 +0400

    'install'

--- service/DanbooruAPI/random_img.py
+++ service/DanbooruAPI/random_img.py
@@ -5,7 +5,9 @@ import time
 basic = HTTPBasicAuth('majajashka', 'yTMKffX5AffivLDNDgYrydrv')
 a = requests.get('https://danbooru.donmai.us/profile.json', auth=basic)
 
-text_pattern = ("Image_url: {img_url}\n")  # 'Tags: <b>{tags}</b>\n'
+text_pattern = ('Rating: <b>{rating}</b>\n'
+                'Score: <b>{score}</b>\n'
+                'Image_url: {img_url}')  # 'Tags: <b>{tags}</b>\n'
 
 
 def random_img(tags: str = '') -> tuple:
@@ -14,9 +16,15 @@ def random_img(tags: str = '') -> tuple:
     }
     random1 = requests.get('https://danbooru.donmai.us/posts/random.json', auth=basic, params=params)
     data = random1.json()
+    print(data)
     img_url = data['file_url']
+    rating = data['rating']
+    score = data['score']
     # tags = data['tag_string']
     # tags = tags.replace(' ', ', ')
-    text = text_pattern.format(img_url=img_url)
+    text = text_pattern.format(rating=rating, score=score, img_url=img_url)
+    print(img_url)
     return text, img_url
 
+
+print(random_img('score:>1000'))
